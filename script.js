dbe7c104befd1be56bdf7e166d4500ca18a35079
Felipe Galdino <alunofelipegaldino9ano@gmail.com>
2024-01-25 20:47:30 -0300

    Função "playRockPaperScissor" adicionada

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2){
+    let pedra = "Pedra"
+    let papel = "Papel"
+    let tesoura = "Tesoura"
+
+    if (player1 == pedra && player2 == pedra || player1 == tesoura && player2 == tesoura || player1 == papel && player2 == papel){
+        return "Empate!"
+    }else if (player1 == pedra && player2 == tesoura || player1== papel && player2 == pedra || player1 == tesoura && player2 == papel){
+        return "Jogador 1 venceu!"
+    }else{
+        return "Jogador 2 venceu!"
+    }
+}
