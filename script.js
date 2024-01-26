aef473e929d2272293b495d519e3d4435faecdd9
Felipe Galdino <alunofelipegaldino9ano@gmail.com>
2024-01-26 15:15:37 -0300

    correção da função playRockPaperScissor

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function playRockPaperScissor(player1, player2){
     let papel = "Papel"
     let tesoura = "Tesoura"
 
-    if (player1 == pedra && player2 == pedra || player1 == tesoura && player2 == tesoura || player1 == papel && player2 == papel){
+    if (player1 == player2){
         return "Empate!"
-    }else if (player1 == pedra && player2 == tesoura || player1== papel && player2 == pedra || player1 == tesoura && player2 == papel){
+    }else if ((player1 == pedra && player2 == tesoura) || (player1== papel && player2 == pedra) || (player1 == tesoura && player2 == papel)){
         return "Jogador 1 venceu!"
     }else{
         return "Jogador 2 venceu!"
